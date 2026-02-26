a9adbf87dc8004550c693919d300023c9cf4ff78
Nina Novakovic-Marinkovic <nina.novakovic13@gmail.com>
2026-02-26 11:33:27 +0100

    top paying job skills

---
+++ SQL_Project_Data_job_Analysis/3_.sql
@@ -0,0 +1,6 @@
+/*
+Question: What are the most in demand skills for my role?
+Question: What are the top skills based on my salary for my role?
+Question: What are the most optimal skills to learn?
+    - Optimal = high demand AND high paying
+*/
