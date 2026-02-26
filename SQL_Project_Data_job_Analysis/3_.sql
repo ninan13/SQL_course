fcc62e144299d12d065d0b47cc1b10ce4f195144
Nina Novakovic-Marinkovic <nina.novakovic13@gmail.com>
2026-02-26 11:55:42 +0100

    Top demanded skills

--- SQL_Project_Data_job_Analysis/3_.sql
+++
@@ -1,6 +0,0 @@
-/*
-Question: What are the most in demand skills for my role?
-Question: What are the top skills based on my salary for my role?
-Question: What are the most optimal skills to learn?
-    - Optimal = high demand AND high paying
-*/
---
+++ SQL_Project_Data_job_Analysis/4_top_paying_skills.sql
@@ -0,0 +1,9 @@
+/*
+Question: What are the top skills based on my salary for my role?
+- Look at the average salary associated with each skill for Data Analyst positions
+- Focus on the roles with specified salaries, regardless of the location
+- Why? It reveals how different skills impact salary levels for Data Analysts and 
+    helps identify the most financially rewarding skills to acquire and improve 
+Question: What are the most optimal skills to learn?
+    - Optimal = high demand AND high paying
+*/
